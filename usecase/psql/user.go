a132d30c6b70060c49799099a878526a124550c1
nobody <kvando.nbd@gmail.com~>
2022-07-11 17:11:49 +0300

    Added handler internal service error

--- usecase/psql/user.go
+++ usecase/psql/user.go
@@ -25,7 +25,7 @@ func (ur UserRepo) Insert(ctx context.Context, u *entity.User) error {
 
 	//TODO: Add internal server error
 	if err != nil {
-		return fmt.Errorf("psql - user - Insert - ur.Pool.Exec: %w", err)
+		return fmt.Errorf("psql - user - Insert - ur.Pool.Exec: %w", fmt.Errorf("%s: %w", err.Error(), entity.ErrInternalService))
 	}
 
 	return nil
@@ -54,7 +54,7 @@ func (ur UserRepo) GetAll(ctx context.Context, fu *entity.FilterUser) ([]entity.
 	rows, err := ur.Pool.Query(ctx, q)
 
 	if err != nil {
-		return nil, fmt.Errorf("psql - user - GetAll - ur.Pool.Query: %w", err)
+		return nil, fmt.Errorf("psql - user - GetAll - ur.Pool.Query: %w", fmt.Errorf("%s: %w", err.Error(), entity.ErrInternalService))
 	}
 
 	for rows.Next() {
@@ -63,7 +63,7 @@ func (ur UserRepo) GetAll(ctx context.Context, fu *entity.FilterUser) ([]entity.
 		err = rows.Scan(&u.Id, &u.FirstName, &u.SecondName, &u.DateOfBirth.Time, &u.IncomePerYear.Icy)
 
 		if err != nil {
-			return nil, fmt.Errorf("psql - user - GetAll - rows.Scan: %w", err)
+			return nil, fmt.Errorf("psql - user - GetAll - rows.Scan: %w", fmt.Errorf("%s: %w", err.Error(), entity.ErrInternalService))
 		}
 
 		us = append(us, *u)
